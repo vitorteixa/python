96b1e980abc34fed59962b228ad83c1947a261ec
Vitor Santos <vitorteixeirasantoss@gmail.com>
2022-02-17 21:57:26 -0300

    conditional commands 2

---
+++ conditional_commands.py
@@ -0,0 +1,10 @@
+varNum1 = 15
+varNum2 = 10
+
+if varNum1 > varNum2 : 
+    print("Num1 é maior que Num2")
+    
+elif varNum2 <  varNum1:
+    print("xablau")
+else:
+    print("Num2 é maior que Num1")
