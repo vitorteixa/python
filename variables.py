ebdffdeb1ec665f122099716f0639a5e87bc0ee6
Vitor Santos <vitorteixeirasantoss@gmail.com>
2022-02-17 21:57:15 -0300

    conditional commands

--- variables.py
+++ variables.py
@@ -1,3 +1,11 @@
 varTesteNum = "1"
 varTesteChar = "hello world"
 print(varTesteChar+" "+varTesteNum)
+
+num1 = 2
+num2 = 3
+
+print(num1+num2) #variables sum
+
+print(num1 ==num2) #comparator
+print(num1 != num2)
