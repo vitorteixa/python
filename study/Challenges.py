a64a9bc9df8f22b0546422b2f7c7c2cd7ab1f29e
vitorteixa <vitorteixeirasantoss@gmail.com>
2022-11-12 23:37:03 -0300

    exercicios e mais aulas

--- study/Challenges.py
+++ study/Challenges.py
@@ -1,4 +1,6 @@
 # Desafio 1
+from _ast import If
+
 nome = input('Qual é o seu nome? ')
 print('Seja bem vindo,', nome, ' é um prazer ter você aqui!')
 
@@ -13,13 +15,22 @@ print('Olá', nome, 'a sua data de nascimento é', dia, '/', mes, '/', ano)
 # Desafio 3
 print('Digite 2 números que serão somados.')
 num1 = int(input())
-num2 = int((input()))
+num2 = float((input()))
 soma = num1 + num2
 print('A soma é:{}'.format(soma))
 
-# validações alfa numerico/ maiuscula
+# Desafio 4 com condicionais
+entrada_teclado = input('Digite algo aqui:')
 
-print(nome.isalpha())
-print(nome.isalnum())
-print(nome.isupper())
-
+if entrada_teclado.isnumeric() is True:
+    print('é numérico')
+else:
+    print('nao é numérico')
+if entrada_teclado.isalnum():
+    print('é alfanumerico')
+else:
+    print('nao é alfanumerico')
+if entrada_teclado.isupper():
+    print('Somente maiúsculas')
+else:
+    print('possui minusculas')
