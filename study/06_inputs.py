3b704f4c7622a9ab76dcf07bf10e1e9b9b1afa55
Vitor Santos <vitorteixeirasantoss@gmail.com>
2022-11-16 23:21:11 -0300

    Teste de commit notebook HP

--- study/06_inputs.py
+++ study/06_inputs.py
@@ -1,18 +1,19 @@
-#Desafio 1
+# Desafio 1
 nome = input('Qual é o seu nome? ')
-print('Seja bem vindo,',nome,' é um prazer ter você aqui!')
+print('Seja bem vindo,', nome, ' é um prazer ter você aqui!')
 
-#Desafio 2
+# Desafio 2
 
 dia = input('Dia do nascimento? ')
 mes = input('Mês do nascimento? ')
 ano = input('Ano do nascimento? ')
 
-print ('Olá', nome, 'a sua data de nascimento é', dia, '/', mes, '/', ano)
+print('Olá', nome, 'a sua data de nascimento é', dia, '/', mes, '/', ano)
 
-#Desafio 3
+# Desafio 3
 
 num1 = 5
 num2 = 6
 
-print ('A soma é:', num1+num2)
+print('A soma é:', num1 + num2)
+print(type(num1))
