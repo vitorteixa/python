4491801da20bc54bfe53f6272519c1c4a44c7259
Vitor Santos <vitorteixeirasantoss@gmail.com>
2022-11-16 23:32:22 -0300

    Adição de exercicios

---
+++ study/Exercises/Ex_03.py
@@ -0,0 +1,7 @@
+# Desafio 3 _ Crie um programa que leia dois números e mostre a soma entre eles.
+
+num1 = float(input('Digite um valor:'))
+num2 = float(input('Digite outro valor:'))
+
+print('A soma dos numeros é:', num1 + num2)
+print(type(num1))
